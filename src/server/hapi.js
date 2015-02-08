c39b962c5019152e7d24ce0388d93341688ca667
Sean Yu <seansu4you87@gmail.com>
2015-02-08 14:34:54 -0800

    Hapi hello world
    - Also added in good logging, seems to be highly recommended.  Only has
      logs to the console right now though, will figure out some kind of
      file logging and log rotation system later

---
+++ src/server/hapi.js
@@ -0,0 +1,33 @@
+var Hapi = require('hapi');
+var Good = require('good');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, reply) {
+    reply('Hello, world!');
+  }
+});
+
+server.register({
+  register: Good,
+  options: {
+    reporters: [{
+      reporter: require('good-console'),
+      args:[{ log: '*', response: '*' }]
+    }]
+  }
+}, function(err) {
+  if (err) {
+    throw err;
+  }
+
+  server.start(function() {
+    server.log('info', 'Server running at: ', server.info.uri);
+  });
+})
+
+module.exports = server;
