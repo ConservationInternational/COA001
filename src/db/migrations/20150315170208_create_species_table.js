4176ff3ee75bc906248fa30459daaf79d2d3ee3f
Sean Yu <seansu4you87@gmail.com>
2015-03-15 17:21:46 -0700

    Adding species table migration
    - Temporarily not including group_id or foot_type_family_id because I
      don't think we need it

--- src/db/migrations/20150315170208_create_species_table.js
+++ src/db/migrations/20150315170208_create_species_table.js
@@ -1,12 +1,29 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
-  //return knex.schema.createTable('species', function(table) {
-  //  table.increments().primary();
-  //  table.uuid('token').unique().notNullable();
-  //
-  //  table.integer('foot_type_family_id').unsigned().references('id').inTable('foot_type_families').notNullable();
-  //})
+  return knex.schema.createTable('species', function(table) {
+    table.increments().primary();
+    table.uuid('token').unique().notNullable();
+
+    //table.integer('foot_type_family_id').unsigned().references('id').inTable('foot_type_families').notNullable();
+    //table.integer('group_id').unsigned().references('id').inTable('groups').notNullable();
+    table.integer('subgroup_id').unsigned().references('id').inTable('subgroups').notNullable();
+
+    table.string('code').notNullable();
+    table.string('name').notNullable();
+
+    table.integer('tarsus_min').notNullable();
+    table.integer('tarsus_max').notNullable();
+    table.integer('wing_min').notNullable();
+    table.integer('wing_max').notNullable();
+    table.integer('bill_min').notNullable();
+    table.integer('bill_max').notNullable();
+
+    table.string('verification_source').notNullable();
+
+    table.boolean('sex_difference').notNullable();
+    table.boolean('active').notNullable();
+  })
 };
 
 exports.down = function(knex, Promise) {
